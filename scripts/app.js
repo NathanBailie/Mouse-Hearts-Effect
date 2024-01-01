814713bc4f8d984611c9c08e70ffdd6f562c7f8f
Nathan <antnefedov33@gmail.com>
2024-01-01 16:53:12 +0300

    wrote basic styles, js variables and functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,31 @@
 "use strict"
+
+let canvas = document.querySelector('.canvas');
+let cts = canvas.getContext("2d");
+let width, height;
+let mouse = { x: undefined, y: undefined };
+let hearts = [];
+
+function start() {
+    resizeReset();
+}
+
+function resizeReset() {
+    width = canvas.width = window.innerWidth;
+    height = canvas.height = window.innerHeight;
+}
+
+function mouseMove(e) {
+    mouse.x = e.x;
+    mouse.y = e.y;
+}
+
+function mouseOut() {
+    mouse.x = undefined;
+    mouse.y = undefined;
+}
+
+window.addEventListener('DOMContentLoaded', start);
+window.addEventListener('resize', resizeReset);
+window.addEventListener('mousemove', mouseMove);
+window.addEventListener('mouseout', mouseOut);
