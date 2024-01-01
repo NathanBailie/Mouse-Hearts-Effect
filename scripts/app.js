7fe2998541391e9493d9dcf1b3bf8ffa29a4d6c7
Nathan <antnefedov33@gmail.com>
2024-01-01 17:58:19 +0300

    wrote update method, updated animationLoop and Heart class, added moving of images and animation of their disappearance

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,8 @@ let mouse = { x: undefined, y: undefined };
 let hearts = [];
 let heartImg = new Image()
 heartImg.src = './assets/icons/heart.png';
+heartImg.width = 60;
+heartImg.height = 60;
 
 function start() {
     resizeReset();
@@ -20,13 +22,24 @@ function resizeReset() {
 
 function animationLoop() {
     ctx.clearRect(0, 0, width, height);
+    ctx.globalCompositeOperation = 'lighter';
+
     drawHearts();
 
+    let temp = [];
+    for (let i = 0; i < hearts.length; i++) {
+        if (hearts[i].time <= hearts[i].lifeTime) {
+            temp.push(hearts[i]);
+        }
+    }
+
+    hearts = temp;
     requestAnimationFrame(animationLoop);
 }
 
 function drawHearts() {
     for (let i = 0; i < hearts.length; i++) {
+        hearts[i].update();
         hearts[i].draw();
     }
 }
@@ -35,7 +48,7 @@ function mouseMove(e) {
     mouse.x = e.x;
     mouse.y = e.y;
 
-    hearts.push(new Heart())
+    hearts.push(new Heart());
 }
 
 function mouseOut() {
@@ -50,23 +63,39 @@ class Heart {
             y: mouse.y + getRandomNum(-20, 20),
             size: heartImg.width,
         }
+
+        this.end = {
+            x: this.start.x + getRandomNum(-200, 200),
+            y: this.start.y + getRandomNum(-650, -350)
+        }
+
         this.x = this.start.x;
         this.y = this.start.y;
         this.size = this.start.size;
+
+        this.time = 0;
+        this.lifeTime = 90;
     }
 
     draw() {
         ctx.drawImage(
             heartImg,
-            this.x,
-            this.y,
+            this.x - heartImg.width / 2,
+            this.y - heartImg.width / 2,
             this.size,
             this.size
         );
     }
 
     update() {
+        if (this.time <= this.lifeTime) {
+            let progress = 1 - (this.lifeTime - this.time) / this.lifeTime;
+            this.size = this.start.size * (1 - easeOutSine(progress));
+            this.x = this.x + (this.end.x - this.x) * 0.009;
+            this.y = this.y + (this.end.y - this.y) * 0.009;
+        }
 
+        this.time++;
     }
 }
 
@@ -78,4 +107,8 @@ window.addEventListener('mouseout', mouseOut);
 
 function getRandomNum(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function easeOutSine(x) {
+    return Math.sin((x * Math.PI) / 2);
 }
